d6fd829ce58b24d5e78a38a26121ed11d5bc3593
Martin Aussenhof <martin@land.tech>
2021-03-06 19:13:44 +0000

    Updates

---
+++ precache-manifest.5cbf9f5b06f01dc7f596aba3293e5f66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "737d8d2bc15fbbbacdee",
+    "url": "/20-buck-chuck-machine/static/css/main.ef34dc9b.chunk.css"
+  },
+  {
+    "revision": "737d8d2bc15fbbbacdee",
+    "url": "/20-buck-chuck-machine/static/js/main.737d8d2b.chunk.js"
+  },
+  {
+    "revision": "18e6cb8c09160be019de",
+    "url": "/20-buck-chuck-machine/static/js/1.18e6cb8c.chunk.js"
+  },
+  {
+    "revision": "0437aa3a6257617a0d8f",
+    "url": "/20-buck-chuck-machine/static/js/runtime~main.0437aa3a.js"
+  },
+  {
+    "revision": "817b33a7c832ec0fa00b71c206a5bc70",
+    "url": "/20-buck-chuck-machine/index.html"
+  }
+];
